32c31cacb57432107ddfeb5edd4e92acb4d63a0a
shim-sham <shambhavisinha2435@gmail.com>
2025-09-08 20:35:12 +0100

    set length as list of inputted notes to account for sharps

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 const input=document.getElementById("input")
 var canvas = document.getElementById("canvas")
 var ctx=canvas.getContext("2d")
+const colourPicker = document.getElementById("colour")
 
 const audioCtx = new AudioContext();
 const gainNode=audioCtx.createGain()
@@ -19,6 +20,7 @@ var height = ctx.canvas.height
 var timePerNote = 0;
 var length=0;
 
+var volSlider = document.getElementById("vol-slider")
 
 noteNames = new Map();
 noteNames.set("c",261.63);
@@ -26,6 +28,7 @@ noteNames.set("c#",277.18);
 noteNames.set("d",293.66);
 noteNames.set("d#",311.13);
 noteNames.set("e",329.63);
+noteNames.set("e#",349.23);
 noteNames.set("f",349.23);
 noteNames.set("f#",369.99);
 noteNames.set("g",392.00);
@@ -38,6 +41,8 @@ noteNames.set("b",493.88);
 function line(){
     y=height/2 + (amplitude * Math.sin(2*Math.PI * freq * x * 0.5 * length))
     ctx.lineTo(x,y);
+    ctx.strokeStyle= colourPicker.value
+
     ctx.stroke()
     x = x+1;
     counter++;
@@ -46,11 +51,17 @@ function line(){
     }
 }
 gainNode.gain.value=0;
+
 function frequency(pitch){
     freq=pitch/10000
-    gainNode.gain.setValueAtTime(50, audioCtx.currentTime);
+    gainNode.gain.setValueAtTime(volSlider.value, audioCtx.currentTime)
+    setting=setInterval(()=> {gainNode.gain.value = volSlider.value}, 1)
+    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime)
     console.log(pitch)
-    oscillator.frequency.setValueAtTime(pitch, audioCtx.currentTime);
+    setTimeout(()=> {
+        clearInterval(setting)
+        gainNode.gain.value = 0
+    },(timePerNote -10))
     gainNode.gain.setValueAtTime(0, audioCtx.currentTime + timePerNote/1000 -0.1);
     
 }
@@ -58,16 +69,11 @@ function handle(){
     reset=true
     var usernotes = String(input.value);
     var noteslist = [];
-    length=usernotes.length
-    timePerNote = 6000/length
-    audioCtx.resume();
-    gainNode.gain.value = 0;
     userInput=String(input.value).toLowerCase()
     for (i = 0; i < usernotes.length; i++) {
         var currentNote = usernotes.charAt(i).toLowerCase()
         if(i < usernotes.length - 1 && usernotes.charAt(i + 1) === '#') {
             var sharpNote = currentNote + '#';
-            console.log(sharpNote)
             if(noteNames.has(sharpNote)) {
                 noteslist.push(noteNames.get(sharpNote));
                 i++;
@@ -79,6 +85,11 @@ function handle(){
         }
         console.log(noteslist)
     }
+
+    length=noteslist.length
+    timePerNote = 6000/length
+    audioCtx.resume();
+    gainNode.gain.value = 0;
     if (noteslist.length>0){
         frequency(parseInt(noteslist[0]))
         drawWave()
